d40d6cf09067808fe921f11b38c469c0aa15d6e9
Aadarsh Jha <Aadarsh.Jha@unisys.com>
2020-04-20 18:29:18 +0530

    Updates

---
+++ precache-manifest.1b8c36f915b07bcbf03719659d465e3c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e05169bef72a65649120628217149379",
+    "url": "/covidinindia/index.html"
+  },
+  {
+    "revision": "1ef28a1993bf7c0438f8",
+    "url": "/covidinindia/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "eb18e34999c1837fd573",
+    "url": "/covidinindia/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "1ef28a1993bf7c0438f8",
+    "url": "/covidinindia/static/js/2.3fcc2f48.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/covidinindia/static/js/2.3fcc2f48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eb18e34999c1837fd573",
+    "url": "/covidinindia/static/js/main.038904d1.chunk.js"
+  },
+  {
+    "revision": "748a7c46de78ce202cbc",
+    "url": "/covidinindia/static/js/runtime-main.439159fc.js"
+  }
+]);
